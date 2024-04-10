f83600934ae27bd1f941ca67504db04578611c3a
janis <janis.boersig@gmx.de>
2024-04-10 14:51:16 +0200

    progress

--- main.py
+++ main.py
@@ -12,15 +12,27 @@ st.title('GKV vs PKV')
 # x range
 years_range = st.slider("Betrachtungszeitraum in Jahren:", THIS_YEAR, A_HUNDRED_YEARS, (THIS_YEAR, THIS_YEAR+75))
 
+st.write("!!!!!BISHER FEHLT BEITRAGS BEMESSUNGSGRENZE!!!!!")
+st.write("!!!!!KEINERLEI BETRACHTUNG VON INFLATION!!!!!")
+
 # input fields
-gkv_percent = st.number_input('GKV Abschlag in % (inklusive Pflegeversicherung) (netto)', min_value=0.0, max_value=100.0, value=19.0/2.0)
-pkv_start = st.number_input('PKV Startkosten in Euro (netto)', min_value=0.0, max_value=1_000.0, value=450.0)
+gkv_base = st.number_input('GKV Abschlag in % (brutto)', min_value=0.0, max_value=100.0, value=14.6)
+gkv_additional = st.number_input('GKV Zusatzbeitrag in % (brutto)', min_value=0.0, max_value=100.0, value=1.3)
+gkv_dental = st.number_input('GKV Zahnarzt in % (brutto)', min_value=0.0, max_value=100.0, value=1.0)
+
+gkv_percent = (
+        gkv_base * 0.5
+        + gkv_additional * 0.5
+        + gkv_dental # employer doesn't pay this
+)
+
+pkv_start = st.number_input('PKV Startkosten in Euro (netto)', min_value=0.0, max_value=1_000.0, value=480.0/2)
 
 gkv_percent_increase = st.number_input('GKV Abschlag Erhöhung pro Jahr in %', min_value=0.0, max_value=100.0, value=3.3)
 pkv_increase = st.number_input('PKV Erhöhung pro Jahr in %', min_value=0.0, max_value=100.0, value=2.6)
 
 income_start = st.number_input('Einkommen Start in Euro', min_value=0.0, max_value=1_000_000.0, value=70_000.0)
-income_increase = st.number_input('Einkommen Erhöhung pro Jahr in %', min_value=0.0, max_value=100.0, value=1.5)
+income_increase = st.number_input('Einkommen Erhöhung pro Jahr in %', min_value=0.0, max_value=100.0, value=2.0)
 
 # Don't forget the price increases for pkv for those two
 pkv_cost_wife = st.number_input('PKV Kosten Ehefrau aktuell in Euro (steigt mit Preisanstieg in Prozent von oben)', min_value=0.0, max_value=1_000.0, value=600.0)
@@ -50,15 +62,15 @@ for year in years:
     # Calculate the PKV cost for the year
     pkv_total_cost = pkv_cost  # Start with base cost for the insured
 
-    # Adjust for spouse PKV cost if within the years she's at home
-    if child_birth_years[0] <= year <= child_birth_years[0] + wife_at_home_years:
-        pkv_total_cost += pkv_cost_wife * 12 # yearly cost
+    if len(child_birth_years) > 0:
+        # Adjust for spouse PKV cost if within the years she's at home
+        if child_birth_years[0] <= year <= child_birth_years[0] + wife_at_home_years:
+            pkv_total_cost += pkv_cost_wife * 12 # yearly cost
 
-
-    # Adjust for children PKV cost
-    for birth_year in child_birth_years:
-        if birth_year <= year <= birth_year+pkv_pay_for_kids_till:
-            pkv_total_cost += pkv_cost_child*12 # yearly cost
+        # Adjust for children PKV cost
+        for birth_year in child_birth_years:
+            if birth_year <= year <= birth_year+pkv_pay_for_kids_till:
+                pkv_total_cost += pkv_cost_child*12 # yearly cost
 
     pkv_costs.append(pkv_total_cost)
 
@@ -93,7 +105,7 @@ st.plotly_chart(fig_yearly)
 fig_cum = px.line(df, x='x')
 fig_cum.add_scatter(x=df['x'], y=df['cum_gkv'], mode='lines', name='Kumulierte GKV Kosten')
 fig_cum.add_scatter(x=df['x'], y=df['cum_pkv'], mode='lines', name='Kumulierte PKV Kosten')
-fig_cum.add_scatter(x=df['x'], y=df['income'], mode='lines', name='Einkommen')
+fig_cum.add_scatter(x=df['x'], y=df['income'], mode='lines', name='Einkommen (nicht kumuliert)')
 fig_cum.update_layout(title='GKV vs PKV Kosten kumulativ', xaxis_title='Jahr', yaxis_title='Kosten in Euro')
 
 st.plotly_chart(fig_cum)
